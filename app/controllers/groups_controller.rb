c0de292a66de61b0d40c0a6f599fc0e6ef7fc2e7
小田哲平 <teppei.oda1992@gmail.com>
2018-10-15 10:34:06 +0900

    Edit groups_controller
    Edit create action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,13 @@ class GroupsController < ApplicationController
     @group.users << current_user
   end
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice:'グループが作成されました'
+    else
+      render :new
+    end
+
   end
   def edit
   end
@@ -12,5 +19,6 @@ class GroupsController < ApplicationController
 
   private
   def group_param
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
