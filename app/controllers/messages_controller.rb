adcb2e0f5d51b97e292a9a1bf5c37f0e6c9bfa0c
小田哲平 <teppei.oda1992@gmail.com>
2018-10-15 15:48:50 +0900

    Make before_action set_group

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :set_group
   def index
-    @group = Group.find(params[:group_id])
     @message = Message.new
   end
   def create
+  end
+
+  private
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
