72453ea57e56b77acde7f14a6ab02c8eb562e90b
小田哲平 <teppei.oda1992@gmail.com>
2018-10-23 13:33:23 +0900

    Make html and add html in message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,14 @@
 $(function(){
+  function buildHAML(message){
+    var html = `<span class='user_name'>${message.name}</span>
+                <span class='updated_at'>${message.updated_at}</span>
+                <div class='body'>${message.body}</div>`
+    return html;
+  }
   $('#new_message').on('submit',function(e){
     e.preventDefault();
-    var formData = new FormData(this)
-    var url = $(this).attr('action')
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -10,6 +16,12 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHAML(data);
+      console.log(html)
+      $('.chat__messages').append(html)
+      $('.text').val('')
     })
   })
 });
