32561c79eec58d9c0119894811c46c2f4b0ed8ca
小田哲平 <teppei.oda1992@gmail.com>
2018-10-26 16:56:54 +0900

    Edit index.js
    Remove console.log

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -17,7 +17,6 @@ $(function(){
     })
     .done(function(users){
       $("#user-search-result").empty();
-      console.log(input);
       if(users.length !== 0){
         users.forEach(function(user){
           appendUser(user);
