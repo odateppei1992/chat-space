9f655dee8b925b08349b4611069858b8077a84d7
小田哲平 <teppei.oda1992@gmail.com>
2018-10-29 13:28:40 +0900

    Edit index.js
    Add function addHTML

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -7,6 +7,14 @@ $(function(){
                 </div>`
   search_list.append(html);
   }
+  function addHTML(id,name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    return html;
+  }
   $("#user-search-field").on("keyup",function(){
     var input = $("#user-search-field").val();
     $.ajax({
@@ -28,6 +36,9 @@ $(function(){
     })
   })
   $(document).on("click",".user-search-add",function(){
-    alert("ok");
+    var id = $(this).data('user-id')
+    var name = $(this).data('user-name')
+    var html = addHTML(id,name);
+    $('.chat-group-users').append(html);
   })
 })
