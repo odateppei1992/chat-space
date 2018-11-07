338b024d07429ce0b91404cc75c63b575274ccb3
小田哲平 <teppei.oda1992@gmail.com>
2018-11-07 16:00:58 +0900

    Edit messages_controller.rb
    Json処理を追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,11 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json{@new_message = @group.messages.includes(:user).where('id>?',params[:message][:id])}
+
+    end
   end
 
   def create
