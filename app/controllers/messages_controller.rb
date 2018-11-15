4c8fff7a6d542e1efac209b8b49e55e4290c9f64
小田哲平 <teppei.oda1992@gmail.com>
2018-11-15 18:38:47 +0900

    Edit messages_controller.rb
    コメントアウトを削除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,7 +17,6 @@ class MessagesController < ApplicationController
         format.json
       end
       flash[:notice] = 'メッセージが送信されました'
-      # redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
